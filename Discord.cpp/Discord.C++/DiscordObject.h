a5198efdd3437239a6453c33d33e58a0ceb9ef20
きんねる <95510722+kinneru@users.noreply.github.com>
2022-04-26 21:41:22 +0900

    Add files via upload

---
+++ Discord.cpp/Discord.C++/DiscordObject.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <cpprest/json.h>
+#include <cpprest/http_client.h>
+
+#ifdef _WIN32
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
+
+namespace DiscordCPP {
+
+	class DiscordObject {
+	protected:
+		///token to authentificate with the discord api
+		utility::string_t _token;
+
+		///helper function to communicate with the http api
+		web::json::value api_call(const std::string& url, const web::http::method& method = web::http::methods::GET, const web::json::value& data = web::json::value(), const std::string& content_type = "", const bool cache = true);
+	public:
+		///the id of the object
+		std::string id;	//snowflake
+
+		DLL_EXPORT DiscordObject();
+		DLL_EXPORT DiscordObject(const utility::string_t& token);
+	};
+
+}
---
+++ Discord.cpp/Discord.C++/User.h
@@ -0,0 +1,68 @@
+#pragma once
+#include "DiscordObject.h"
+
+#include <iostream>
+
+#include "Logger.h"
+
+#ifdef _WIN32
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
+
+namespace DiscordCPP {
+
+	using namespace std;
+	using namespace web::json;
+	using namespace utility;
+
+	class Message;
+	class Embed;
+	class DMChannel;
+
+	class User : public DiscordObject {
+	public:
+		enum PremiumTypes {
+			None,
+			NitroClassic,
+			Nitro
+		};
+
+		///ther user's name
+		string username;
+		///the user's discord-tag
+		string discriminator;
+		///the user's avatar hash
+		string avatar;
+		///true, if the user is a bot
+		bool bot = false;
+		///true, if the user has enabled two factor authentification
+		bool mfa_enabled = false;
+		///the user's chosen language option
+		string locale;
+		///true, if the user's email has been verified
+		bool verified = false;
+		///the user's email
+		string email;
+		///the flags on a user's account
+		int flags = 0;
+		///the type of Nitro subscription on a user's account
+		int premium_type = PremiumTypes::None;
+
+		DLL_EXPORT User();
+		DLL_EXPORT User(const value& data, const string_t& token);
+		DLL_EXPORT User(const string& id, const string_t& token);
+
+		///get/create the DMChannel for this user
+		DLL_EXPORT DMChannel get_dmchannel();
+		///send a message to this user
+		DLL_EXPORT Message send(const string& content, const bool tts = false);
+		///send an embed to this user
+		DLL_EXPORT Message send(const Embed& embed);
+
+		///@return Username as std::string
+		DLL_EXPORT virtual operator string() { return username; };
+	};
+
+}
